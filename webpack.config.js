89e9c70dcd1916bcbf21b1406e1e45a179c1d537
Diego Matias <dm.diego.bh@gmail.com>
2018-12-02 13:35:33 -0200

    changes on 02-12-2018

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,36 @@
 const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require('webpack');
+
 
 module.exports = {
   entry: './unified.js',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      }
+    ]
+  },
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    library: 'myLibrary',
+    libraryTarget: 'var'
   },
   externals: {
     // require("jquery") is external and available
     //  on the global var jQuery
-    "jquery": "jQuery"
-  }
+    jquery: 'jQuery'
+  },
+  optimization: {
+    minimizer: [new UglifyJsPlugin()]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+                $: "jquery",  
+                jQuery: "jquery" 
+            })
+    ],
 };
